52549638cf39c7c996ca12e0dcd51b8314f6fa0f
Victor Santo Barcelos <victor61673226@edu.df.senac.br>
2026-05-19 16:56:34 -0300

    Commit do banner.

---
+++ sintex/src/Componentes/Banner/Banner.jsx
@@ -0,0 +1,8 @@
+export default function Banner() {
+    return (
+        <div className="banner">
+            <h1>Teste</h1>
+            <p>O melhor site de notícias de tecnologia do Brasil</p>
+        </div>
+    )
+}
