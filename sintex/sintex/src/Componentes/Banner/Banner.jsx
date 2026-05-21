526d08be7cf35c3e98b757b974d8f2859cff9a1c
Victor Santo Barcelos <victor61673226@edu.df.senac.br>
2026-05-21 15:41:15 -0300

    Victor: parte do banner, imagens e cores

--- sintex/sintex/src/Componentes/Banner/Banner.jsx
+++ sintex/sintex/src/Componentes/Banner/Banner.jsx
@@ -10,7 +10,6 @@ const Banner = () => {
         <span className="banner-badge">GASTRONOMIA MUNDIAL</span>
         <h1>Uma Viagem de <br /><span>Sabores Incomparáveis</span></h1>
         <p>Da delicadeza da culinária japonesa à força dos pratos alemães, italianos e do nosso sabor brasileiro.</p>
-        <button className="btn-pedir">Explorar Cardápio</button> 
       </div> 
 
       {/* Lado Direito: O Vídeo */}
--- sintex/sintex/src/Componentes/Menu/Menu.jsx
+++ sintex/sintex/src/Componentes/Menu/Menu.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import MenuLink from "../MenuLink/MenuLink";
 import "./Menu.css";
+import logo from "./imagens/logo.png";
 
 // Menu principal com links e busca de restaurantes.
 export default function Menu() {
@@ -24,6 +25,9 @@ export default function Menu() {
 
   return (
     <nav className="Menu">
+      <div className="MenuLogo">
+        <img src={logo} alt="Sintex Logo" className="MenuLogoImage" />
+      </div>  
       <div className="MenuLinks">
         <MenuLink to="/">Home</MenuLink>
         <MenuLink to="/about">About</MenuLink>
